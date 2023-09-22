702e24b980fc5d7eefb6abf61e0c6be6f79c101d
“zzk” <2814318648@qq.com>
2023-09-23 01:30:28 +0800

    登录页面编写

--- blog_general/src/pages/Login.vue
+++ blog_general/src/pages/Login.vue
@@ -1,13 +1,65 @@
 <template>
+<div>登录页面</div>
+  <div class="login-box">
+  <el-form
+      ref="ruleFormRef"
+      :model="ruleForm"
+      status-icon
+      :rules="rules"
+      label-width="120px"
+      class="demo-ruleForm"
+  >
+    <el-form-item label="username" prop="username">
+      <el-input v-model="ruleForm.username" type="username" autocomplete="off" />
+    </el-form-item>
+    <el-form-item label="password" prop="password">
+      <el-input
+          v-model="ruleForm.password"
+          type="password"
+          autocomplete="off"
+      />
+    </el-form-item>
 
+    <el-form-item>
+      <el-button type="primary" @click="submitForm(ruleFormRef)"
+      >Submit</el-button
+      >
+      <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
+    </el-form-item>
+  </el-form>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "Login"
-}
+<script lang="ts">
+import {defineComponent, reactive, toRefs} from "vue";
+
+export default defineComponent({
+
+    name: "Login",
+    setup(){
+      const data=reactive({
+        ruleForm:{
+          username:"",
+          password:""
+        },
+        rules:{
+          username: [
+            { required: true, message: '', trigger: 'blur' },
+            { min: 3, max: 5, message: 'Length should be 3 to 10', trigger: 'blur' },
+          ],
+          password: [
+            { required: true, message: '', trigger: 'blur' },
+            { min: 3, max: 5, message: 'Length should be 3 to 10', trigger: 'blur' },
+          ],
+        }
+      });
+      return {...toRefs(data)};
+  }
+})
+
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
 
 </style>
