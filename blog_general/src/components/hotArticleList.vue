6af24541af2b938a3c96494a1bb4c14656220bd7
“zzk” <2814318648@qq.com>
2023-10-09 10:57:25 +0800

    实现热门文章列表接口

---
+++ blog_general/src/components/hotArticleList.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-card class="box-card">
+    <template #header>
+      <div  v-for="item in articles" class="card-header">
+        <span>{{ item.title }}</span>
+        <div class="viewCounts">有{{ item.viewcounts }}人看过啦</div>
+      </div>
+    </template>
+  </el-card>
+</template>
+
+<script lang="ts">
+import { hotArticleList } from "@/api/article";
+import {defineComponent, onMounted, reactive, toRefs} from "vue";
+
+export default defineComponent({
+  name: "hotArticleList",
+  setup(){
+    const data=reactive({
+      articles:[]
+    })
+    onMounted(()=>{
+      hotArticleList().then((res)=>{
+        // console.log(res.data);
+        data.articles=res.data
+        console.log(data.articles)
+      })
+    })
+    return{...toRefs(data),}
+  }
+})
+
+
+
+</script>
+
+<style scoped>
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.box-card {
+  width: 480px;
+}
+</style>
