e9ec4080ac201ce3fe4f981ff92f182dcb1153de
bibiudang <2814318648@qq.com>
2023-10-15 01:38:26 +0800

    实现时间线列表组件，修改部分导航栏

--- blog_general/src/pages/Articles.vue
+++ blog_general/src/pages/Articles.vue
@@ -1,11 +1,57 @@
 <template>
+  <div>
+    <Header></Header>
+    </div>
+  <div class="timelinelist">
+    <el-timeline >
+      <el-timeline-item v-for="article in articles"
+                        placement="top"
+                        :timestamp=article.createtime>
+        <el-card>
+          <h4 @click="getDetail(article.id)"> {{article.title}}</h4>
+          <h5><p>{{ article.discription }}</p></h5>
+        </el-card>
+      </el-timeline-item>
 
+    </el-timeline>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "Articles"
-}
+<script lang ="ts">
+import {defineComponent, onMounted, reactive, toRefs} from "vue";
+import {ArticleListData} from "@/types";
+import Header from "@/components/myHeader.vue";
+import {useRouter} from "vue-router";
+import {articleList} from "@/api/article";
+
+export default defineComponent({
+  name: "Articles",
+  components:{Header},
+  setup(){
+    const data=reactive({
+      articles:[],
+      param:{
+        pageNum:1,
+        pageSize:1000,
+        categoryId:0
+      } as ArticleListData,
+    })
+    const router=useRouter()
+    function getDetail(index:number){
+      router.push("/articleDetail/"+index)
+    }
+    onMounted(()=>{
+      articleList(data.param).then((res)=>{
+        console.log(res.data)
+        data.articles=res.data.rows
+      })
+    })
+
+    return{...toRefs(data),getDetail}
+  }
+})
+
+
 </script>
 
 <style scoped>
